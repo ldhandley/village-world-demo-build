a2e1c82d4dd40ca6dc935a3b13899556f615dccd
srfoster <lindseyhandley@gmail.com>
2020-11-25 16:53:06 -0800

    updated lore

--- lore.rkt
+++ lore.rkt
@@ -3,14 +3,21 @@
 (require village-world-demo-build
 	 codespells/lore)
 
-(define-rune-collection-lore 
-  #:name "A Cool Name for VillageWorldDemoBuild"
+(define-authored-work-lore
+  #:name "Village"
   #:description 
   @md{
-    A cool intro about VillageWorldDemoBuild
+    The @b{Village of Acostia} was designed by @b{Tomh the Enchanter}. This playful little village is often used as a setting in beginning spellcrafting courses at the @b{Low Stacks Academy}. 
+    
+    @(div
+      (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Try skipping rocks on the nearby pond!"))
   }
-  #:rune-lores
-  (list)
+  #:rune-collections
+  (list 'hierarchy
+        'spawners
+        'fire-particles
+	'ice-particles
+	'rocks)
   #:preview-image preview.png)
 
 
